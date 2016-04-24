05490cc6bcb841336411ebd136b7eea32f4346f4
shangril <nicolas.chartoire@gmail.com>
2016-04-24 16:17:29 +0200

    added caching for cover art thumbnails to speed up page load

--- thumbnailer.php
+++ thumbnailer.php
@@ -1,5 +1,5 @@
 <?php
- 
+include ('./config.php');
  
   if (!isset($_GET['target'])&&!isset($_GET['viewportwidth'])&&!isset($_GET['ratio'])){
 	  die();
@@ -10,17 +10,51 @@
   $ratio=$_GET['ratio'];
   
   if (file_exists('./'.$file) && strpos(mime_content_type('./'.$file),'image/')==0){ 
-	  	  if (isset($_SERVER['HTTP_IF_MODIFIED_SINCE']) && 
+/*	  	  if (isset($_SERVER['HTTP_IF_MODIFIED_SINCE']) && 
     strtotime($_SERVER['HTTP_IF_MODIFIED_SINCE']) >= filemtime('./'.$file)
     &&$ratio<0.5)
 		{
 			header('HTTP/1.0 304 Not Modified');
 			exit;
 		}  
+*/
 	header('Content-type: application/x-png'); 
 	list($width, $height) = getimagesize($file);
 	$modwidth=floatval($ratio)*(floatval($viewportwidth)); 
 	$modheight = $modwidth;
+	if (!isset($_GET['hook'])){
+	if (file_exists('./thumbcache/'.$modwidth.'-'.$modheight.'-'.str_replace('/','',$file).'.png')){
+		  if (isset($_SERVER['HTTP_IF_MODIFIED_SINCE']) && 
+			strtotime($_SERVER['HTTP_IF_MODIFIED_SINCE']) >= filemtime('./thumbcache/'.$modwidth.'-'.$modheight.'-'.str_replace('/','',$file).'.png')
+			)
+			{
+				header('HTTP/1.0 304 Not Modified');
+				exit;
+			}  
+			
+			
+			$handle=fopen('./thumbcache/'.$modwidth.'-'.$modheight.'-'.str_replace('/','',$file).'.png', 'rb');
+			
+			fpassthru($handle);
+			fclose($handle);
+			die();
+			
+		}
+		else {
+			file_put_contents('./thumbcache/'.$modwidth.'-'.$modheight.'-'.str_replace('/','',$file).'.png', file_get_contents(
+			'http://'.$server.'/thumbnailer.php?hook=hook&ratio='.urlencode($_GET['ratio']).'&target='.urlencode($_GET['target']).'&viewportwidth='.urlencode($_GET['viewportwidth'])
+			));
+			$handle=fopen('./thumbcache/'.$modwidth.'-'.$modheight.'-'.str_replace('/','',$file).'.png', 'rb');
+			
+			fpassthru($handle);
+			fclose($handle);
+			die();
+
+		}
+		
+	}
+
+
 	$output= imagecreatetruecolor($modwidth, $modheight); 
 	$type=mime_content_type('./'.$file);
 		if (strstr($type,  'gif')){
@@ -37,7 +71,12 @@
 			
 		}
 		imagecopyresized($output, $source, 0, 0, 0, 0, $modwidth, $modheight, $width, $height);
-		imagepng($output);
+
+
+		if (isset($_GET['hook'])){
+			
+			imagepng($output);
+		} 
 }
 die();	   
 ?>
