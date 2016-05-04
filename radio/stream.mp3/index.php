921cf664487eef3ea4d06babe0b715ebed293b3e
shangril <nicolas.chartoire@gmail.com>
2016-05-04 13:03:25 +0200

    radio : better balance between featured and base track + better timing for sending stream

--- radio/stream.mp3/index.php
+++ radio/stream.mp3/index.php
@@ -168,7 +168,74 @@ $nextbitrate=$nowplayingbitrate;
 $nexttitle=$nowplayingtitle;
 
 if (microtime(true)>$expire){
-		if (file_exists('../d/ypsid.txt')&&floatval(trim(file_get_contents('../d/ypexpires.txt')))<microtime(true)){
+		
+	
+	$dice=rand(1,10);
+	if ($dice==1){
+		$featured=explode("\n", $radiofeatured);
+		shuffle($featured);
+		//$thisfeatured=$featured[0];
+		$thisfeatured = $featured[mt_rand(0, count($featured) - 1)];
+		$featuredbasenamed=explode('/', $thisfeatured);
+		$featuredbasename=array_pop($featuredbasenamed);
+		$apihook=str_replace($featuredbasename, '', $thisfeatured);
+		$apihook=str_replace('z/', 'api.php', $apihook);
+		$apihook.='?radio='.urlencode($featuredbasename);
+		$apirequest=file_get_contents($apihook);
+		if (count($apirequest)>0){
+			$result=explode("\n", $apirequest);
+			
+			$nexturl=$thisfeatured;
+			$nextartist=$result[0];
+			$nextalbum=$result[1];
+			$nexttitle=$result[2];
+			$nextduration=$result[3];
+			$nextbitrate=$result[4];
+			file_put_contents('../d/nowplayingisfeatured.txt', '0');
+			file_put_contents('../d/starttime.txt', microtime(true));
+		}
+	}
+	else {
+		$featured=explode("\n", $radiobase);
+		shuffle($featured);
+		//$thisfeatured=$featured[0];
+		$thisfeatured = $featured[mt_rand(0, count($featured) - 1)];
+		
+		$featuredbasenamed=explode('/', $thisfeatured);
+		$featuredbasename=array_pop($featuredbasenamed);
+		
+		$apihook=str_replace($featuredbasename, '', $thisfeatured);
+		$apihook=str_replace('audio/', 'api.php', $apihook);
+		$apihook.='?radio='.urlencode($featuredbasename);
+		$apirequest=file_get_contents($apihook);
+		$result=explode("\n", $apirequest);
+		
+		$nexturl=$thisfeatured;
+		$nextartist=$result[0];
+		$nextalbum=$result[1];
+		$nexttitle=$result[2];
+		$nextduration=$result[3];
+		$nextbitrate=$result[4];
+		file_put_contents('../d/nowplayingisfeatured.txt', '1');
+		file_put_contents('../d/starttime.txt', microtime(true));
+		
+	}
+$nowplayingduration=$nextduration;
+
+file_put_contents('../d/nowplayingduration.txt', $nowplayingduration);
+$nowplayingurl=$nexturl;
+file_put_contents('../d/nowplayingurl.txt', $nowplayingurl);
+$nowplayingalbum=$nextalbum;
+file_put_contents('../d/nowplayingalbum.txt', $nowplayingalbum);
+$nowplayingtitle=$nexttitle;
+file_put_contents('../d/nowplayingtitle.txt',$nowplayingtitle);
+$nowplayingartist=$nextartist;
+file_put_contents('../d/nowplayingartist.txt', $nowplayingartist);
+$expire=floatval(microtime(true))+floatval($nextduration);
+file_put_contents('../d/expire.txt', $expire);
+$nowplayingbitrate=$nextbitrate;
+file_put_contents('../d/nowplayingbitrate.txt',$nowplayingbitrate);
+	if (file_exists('../d/ypsid.txt')&&floatval(trim(file_get_contents('../d/ypexpires.txt')))<microtime(true)){
 			$sid=file_get_contents('../d/ypsid.txt');
 			$nowplaying=file_get_contents('../d/nowplayingartist.txt').' - '.file_get_contents('../d/nowplayingtitle.txt');
 			
@@ -210,73 +277,6 @@ if (microtime(true)>$expire){
 				file_put_contents('../d/ypexpires.txt', microtime(true)+floatval(file_get_contents('../d/ypttl.txt')));
 			}
 		}
-	
-	$dice=rand(1,10);
-	if ($dice==1){
-		$featured=explode("\n", $radiofeatured);
-		//shuffle($featured);
-		//$thisfeatured=$featured[0];
-		$thisfeatured = $featured[mt_rand(0, count($featured) - 1)];
-		$featuredbasenamed=explode('/', $thisfeatured);
-		$featuredbasename=array_pop($featuredbasenamed);
-		$apihook=str_replace($featuredbasename, '', $thisfeatured);
-		$apihook=str_replace('z/', 'api.php', $apihook);
-		$apihook.='?radio='.urlencode($featuredbasename);
-		$apirequest=file_get_contents($apihook);
-		if (count($apirequest)>0){
-			$result=explode("\n", $apirequest);
-			
-			$nexturl=$thisfeatured;
-			$nextartist=$result[0];
-			$nextalbum=$result[1];
-			$nexttitle=$result[2];
-			$nextduration=$result[3];
-			$nextbitrate=$result[4];
-			file_put_contents('../d/nowplayingisfeatured.txt', '0');
-			file_put_contents('../d/starttime.txt', microtime(true));
-		}
-	}
-	else {
-		$featured=explode("\n", $radiobase);
-		//shuffle($featured);
-		//$thisfeatured=$featured[0];
-		$thisfeatured = $featured[mt_rand(0, count($featured) - 1)];
-		
-		$featuredbasenamed=explode('/', $thisfeatured);
-		$featuredbasename=array_pop($featuredbasenamed);
-		
-		$apihook=str_replace($featuredbasename, '', $thisfeatured);
-		$apihook=str_replace('audio/', 'api.php', $apihook);
-		$apihook.='?radio='.urlencode($featuredbasename);
-		$apirequest=file_get_contents($apihook);
-		$result=explode("\n", $apirequest);
-		
-		$nexturl=$thisfeatured;
-		$nextartist=$result[0];
-		$nextalbum=$result[1];
-		$nexttitle=$result[2];
-		$nextduration=$result[3];
-		$nextbitrate=$result[4];
-		file_put_contents('../d/nowplayingisfeatured.txt', '1');
-		file_put_contents('../d/starttime.txt', microtime(true));
-		
-	}
-$nowplayingduration=$nextduration;
-
-file_put_contents('../d/nowplayingduration.txt', $nowplayingduration);
-$nowplayingurl=$nexturl;
-file_put_contents('../d/nowplayingurl.txt', $nowplayingurl);
-$nowplayingalbum=$nextalbum;
-file_put_contents('../d/nowplayingalbum.txt', $nowplayingalbum);
-$nowplayingtitle=$nexttitle;
-file_put_contents('../d/nowplayingtitle.txt',$nowplayingtitle);
-$nowplayingartist=$nextartist;
-file_put_contents('../d/nowplayingartist.txt', $nowplayingartist);
-$expire=floatval(microtime(true))+floatval($nextduration);
-file_put_contents('../d/expire.txt', $expire);
-$nowplayingbitrate=$nextbitrate;
-file_put_contents('../d/nowplayingbitrate.txt',$nowplayingbitrate);
-	
 }
 $filepath='';
 if (strstr( $nowplayingurl, 'clewn.org')){
@@ -313,8 +313,10 @@ $beta=microtime(true)-$alpha;
 
 if (floatval(microtime(true))<floatval($expire)){
 
-$timetosleep=1000000+(1000000*(floatval($nowplayingduration)-(floatval(microtime(true))-floatval($hasstarted))))-(1000000*$beta);
+$timetosleep=(1000000*(floatval($nowplayingduration)-(floatval(microtime(true))-floatval($hasstarted))))-(1000000*$beta);
 $sleeped=0;
+
+
 
 while ($timetosleep-$sleeped>5000000){
 
@@ -331,10 +333,11 @@ usleep ($timetosleep-$sleeped);
 
 }
 
-//while (floatval(microtime(true))<floatval($expire)){
-//	sleep(5);
-//}
+	//while (floatval(microtime(true))<floatval($expire)){
+	//	sleep(5);
+	//}
 if (!isset($_GET['web'])){
+
 	play($radioname, $server, $radiodescription, $labelgenres, $radiohasyp);
 }
 
