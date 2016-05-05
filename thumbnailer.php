45e2aaccce4ac0f8f120ebb2aa99810269cf5ba6
shangril <nicolas.chartoire@gmail.com>
2016-05-05 08:30:11 +0200

    Vulnerability fix (thanks xcombelle) : thumbnailer.php allowed an specially crafted GET request to thumbnail images outside of the covers/ directory. Fixed. No other filetypes than images where allowed to be accessed

--- thumbnailer.php
+++ thumbnailer.php
@@ -9,7 +9,7 @@ include ('./config.php');
   $viewportwidth= $_GET['viewportwidth'];
   $ratio=$_GET['ratio'];
   
-  if (file_exists('./'.$file) && strpos(mime_content_type('./'.$file),'image/')==0){ 
+  if (file_exists('./'.$file) && strpos(mime_content_type('./'.$file),'image/')==0&&dirname(realpath($file))===realpath('./covers')){ 
 /*	  	  if (isset($_SERVER['HTTP_IF_MODIFIED_SINCE']) && 
     strtotime($_SERVER['HTTP_IF_MODIFIED_SINCE']) >= filemtime('./'.$file)
     &&$ratio<0.5)
