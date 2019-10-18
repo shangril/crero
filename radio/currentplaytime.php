4d54c646c8bd7b4ad8a1ed3070c18c2f3390d2b7
Nico <nicolas.chartoire@gmail.com>
2019-10-18 18:58:11 +0200

    BUGFIX

--- radio/currentplaytime.php
+++ radio/currentplaytime.php
@@ -5,7 +5,14 @@ $offset=microtime(true)-floatval($_GET['start']);
 
 $starttime = floatval(trim(file_get_contents('./d/starttime.txt')));
 $duration = floatval(trim(file_get_contents('./d/nowplayingduration.txt')));
-echo  microtime(true)-$starttime-($duration-(microtime(true)-$starttime)+floatval($_GET['current']))+($starttime-$_SESSION ['streamhit'])+$offset;
+/*echo  microtime(true)-$starttime
+-($duration-(microtime(true)-$starttime)+floatval($_GET['current']))
++($starttime-floatval($_SESSION ['streamhit']))
++$offset;
+*/
+echo ($duration-(microtime(true)-$starttime)-($starttime-floatval($_SESSION ['streamhit'])-floatval($_GET['current'])-$offset))-(microtime(true)-$starttime);
+
+
 exit(0);
 
 ?>
